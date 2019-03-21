17a9030f0e3699d4db5ac0218a9dbc342549b066
Juhanir <juhani.riisio@gmail.com>
2019-03-21 13:30:12 +0200

    integrated auddio api request to humming

--- src/serviceClient.js
+++ src/serviceClient.js
@@ -1,5 +1,5 @@
-// Module for ASYNC resource calls from client
-
+// postFile returns { success:true, message: array} in successful cases (array is empty if no results)
+// postFile returns { success:false, message: error message} in unsuccessful cases
 export async function postFile(data) {
     const formData = new FormData();
     formData.append('audiofile', data);
@@ -10,11 +10,28 @@ export async function postFile(data) {
         body: formData
     };
     try {
-        let res = await fetch('http://localhost:3001/', options);
-        if (!res.ok) throw Error(res.status);
-        return true;
+        let res = await fetch('http://localhost:3001/hum', options);
+        let jsonRes = await res.json();
+        if (jsonRes.success) {
+            return jsonRes.message;
+        } else {
+            throw new Error(jsonRes.message);
+        }
     } catch (error) {
-        console.error('File upload failed: ' + error);
-        throw false;
+        console.error(error);
+        return error.message;
+    }
+}
+
+export async function mockResult() {
+    try {
+        let res = await fetch('http://localhost:3001/hum');
+        if (!res.ok) throw Error(res.status);
+        let jsonRes = await res.json();
+        if (!jsonRes.success) throw Error(jsonRes.message);
+        return JSON.parse(jsonRes.message);
+    } catch (error) {
+        console.error('result mock getter failed: ' + error);
+        return false;
     }
 }

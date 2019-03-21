cd6badd0b9859cb32cba381376eed96dad045334
jiiaa <jukkaa@live.com>
2019-03-21 17:07:03 +0200

    spinner component added /Jukka

--- src/components/filecomponents/FileTest.js
+++ src/components/filecomponents/FileTest.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Latency from '../Latency';
 
 class FileTest extends Component {
     render() {
         return (
             <div>
                 <p>File testausta</p>
+                <Latency />
             </div>
         );
     }

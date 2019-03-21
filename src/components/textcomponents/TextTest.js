fe5c9b1eb736bd5c0b78e0bd2498f70d6136e795
obrummer <obrummer@gmail.com>
2019-03-21 16:15:32 +0200

    tekstihakuun lisätty datahaun muotoilu

--- src/components/textcomponents/TextTest.js
+++ src/components/textcomponents/TextTest.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Card } from 'react-bootstrap';
+import LyricBox from './LyricBox';
+import './styles/TextTest.css';
 
 class TextTest extends Component {
     constructor(props) {
@@ -8,18 +10,6 @@ class TextTest extends Component {
     }
 
     state = { url: '/', data: [] }
-
-    // componentDidMount = () => {
-    //     this.gettextrecognition();
-    // }
-    // gettextrecognition = () => {
-    //     fetch(this.state.url, { mode: "cors" })
-    //         .then(vastaus => vastaus.json())
-    //         .then(data => {
-    //             this.setState({ data: data });
-    //             console.log(data);
-    //         })
-    // }
 
     handleSubmit(e) {
         e.preventDefault();
@@ -32,57 +22,61 @@ class TextTest extends Component {
         var endresult = JSON.parse(result);
         var res = encodeURI(endresult.text);
 
-        var url = "https://api.audd.io/findLyrics/?q=" + res;
+        var url = "https://audd.p.rapidapi.com/findLyrics/?q=" + res;
         console.log(url);
-        var data = { url: url};
+        var data = { url: url };
 
-        fetch("http://loppuprojekti-env.4wv6cxwtgr.eu-central-1.elasticbeanstalk.com/lyrics", 
-        {
-            mode: 'cors',
-            method: 'POST', 
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers:{
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-            }
-          })
-          .then(res => res.json())
-          .then(response => 
-            console.log('Success:', response))
-          .catch(error => 
-            console.error('Error:', error));
-
-        // fetch(url, { mode: "cors" })
-        //     .then(vastaus => vastaus.json())
-        //     .then(data => {
-        //         this.setState({ data: data });
-        //         console.log(data);
-        //     })
+        fetch("http://loppuprojekti-env.4wv6cxwtgr.eu-central-1.elasticbeanstalk.com/lyrics",
+            {
+                mode: 'cors',
+                method: 'POST',
+                body: JSON.stringify(data), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(response => {
+                console.log('Success:', response)
+                this.setState({ data: response.message.result })
+            })
     }
 
-    // state = { url: "https://api.audd.io/findLyrics/?q=I%27m%20waking%20up%20to%20ash%20and%20dust", data: [] }  
-
     render() {
-        return (
-            <div>
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <input ref="text" className="text" name="text" />
-                        <input type="submit" value="Test" />
-                    </form>
-                </div>
-                {/* <Form>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Write your lyrics</Form.Label>
-                        <Form.Control as="textarea" rows="2" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Test
-                    </Button>
-                </Form> */}
+        const content = this.state.data.map((s, index) =>
+            <div key={index}>
+                <Card>
+                    <Card.Header>Artist: {s.artist}</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Song title: {s.title}</Card.Title>
+                        <div>
+                            <LyricBox text={s.lyrics}/>
+                        </div>
+                        {/* <Button variant="primary">Get lyrics</Button> */}
+                    </Card.Body>
+                </Card>
             </div>
+        )
+
+        return (
+                <div>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="exampleForm.ControlTextarea1" className="textfield">
+                            <Form.Label>Write your lyrics</Form.Label>
+                            <Form.Control type="text" placeholder="" ref="text" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Test
+                    </Button>
+                    </Form>
+                    <ul>
+                        {content}
+                    </ul>
+                </div>
         );
     }
 }
 
 export default TextTest;
+

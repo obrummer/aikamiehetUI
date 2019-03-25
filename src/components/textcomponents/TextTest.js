71439eb340103e11fdfa9894a85d03f0b6a16045
obrummer <obrummer@gmail.com>
2019-03-25 13:24:51 +0200

    no textmatch lisätty

--- src/components/textcomponents/TextTest.js
+++ src/components/textcomponents/TextTest.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { Button, Form, Card } from 'react-bootstrap';
+import { Button, Form, Card, Col, Row } from 'react-bootstrap';
 import LyricBox from './LyricBox';
 import Latency from '../Latency';
+import NoTextMatch from './NoTextMatch';
 import './styles/TextTest.css';
+
 
 class TextTest extends Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { url: '/', data: [], open: false, loading: false }
     }
 
-    state = { url: '/', data: [], open: false, loading: false }
 
     handleSubmit(e) {
         e.preventDefault();
@@ -46,35 +48,50 @@ class TextTest extends Component {
     }
 
     render() {
+
         const content = this.state.data.map((s, index) =>
             <div key={index}>
-                <Card>
-                    <Card.Header>Artist: {s.artist}</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Song title: {s.title}</Card.Title>
-                        <div>
-                            <LyricBox text={s.lyrics}/>
-                        </div>
-                    </Card.Body>
-                </Card>
+                <Row>
+                    <Col md={3} />
+                    <Col md={6}>
+                        <Card>
+                            <Card.Header>Artist: {s.artist}</Card.Header>
+                            <Card.Body>
+                                <Card.Title>Song title: {s.title}</Card.Title>
+                                <div>
+                                    <LyricBox text={s.lyrics} />
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={3} />
+                </Row>
             </div>
         )
 
         return (
-                <div>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group controlId="exampleForm.ControlTextarea1" className="textfield">
-                            <Form.Label>Write your lyrics</Form.Label>
-                            <Form.Control type="text" placeholder="" ref="text" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Test
+            <div>
+                <Row>
+                <Col md={3} />
+                <Col md={6}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="exampleForm.ControlTextarea1" className="textfield">
+                        <Form.Label>Write your lyrics</Form.Label>
+                        <Form.Control as="textarea" rows="1" type="text" placeholder="" ref="text" required />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Test
                     </Button>
-                    </Form>
-                    <br />
-                    { this.state.loading ? <Latency /> : '' }
-                    {content}
-                </div>
+                </Form>
+                </Col>
+                <Col md={3} />
+                </Row>
+                <br />
+                {this.state.loading ? <Latency /> : ''}
+                {this.state.data.length === 0 ? <NoTextMatch /> : content}
+                {/* { this.state.loading===true ? <NoTextMatch style={{ display: 'none' }} /> : <NoTextMatch /> } */}
+                <br />
+            </div>
         );
     }
 }

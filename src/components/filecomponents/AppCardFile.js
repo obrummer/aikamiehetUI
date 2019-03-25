a80f0a167ac081611609b93fff65e75612e79dbe
Juhanir <juhani.riisio@gmail.com>
2019-03-25 08:47:40 +0200

    picture file location changed

--- src/components/filecomponents/AppCardFile.js
+++ src/components/filecomponents/AppCardFile.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import filepic from './styles/filepic.png';
 
 class AppCardFile extends Component {
     render() {
         return (
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={filepic} />
+                <Card.Img variant="top" src="filepic.png" />
                 <Card.Body>
                     <Card.Title>Test audiofile</Card.Title>
-                    <Card.Text>
-                        Test your produced audiofile and get matching results.
-              </Card.Text>
-              <Link to="/filetest"><Button variant="primary">Test</Button></Link>
+                    <Card.Text>Test your produced audiofile and get matching results.</Card.Text>
+                    <Link to="/filetest">
+                        <Button variant="primary">Test</Button>
+                    </Link>
                 </Card.Body>
             </Card>
         );
--- src/components/humcomponents/AppCardHum.js
+++ src/components/humcomponents/AppCardHum.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import testpic from './styles/testpic.png';
 
 class AppCardHum extends Component {
     render() {
         return (
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={testpic} />
+                <Card.Img variant="top" src="testpic.png" />
                 <Card.Body>
                     <Card.Title>Test by humming</Card.Title>
                     <Card.Text>Test you song by humming and get matching results.</Card.Text>

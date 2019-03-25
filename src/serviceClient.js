ef9bb867432ef16edb33eee920435ec976242d75
Juhanir <juhani.riisio@gmail.com>
2019-03-25 16:49:53 +0200

    text form loading screen and other stuff modified

--- src/serviceClient.js
+++ src/serviceClient.js
@@ -20,3 +20,28 @@ export async function postFile(data, path) {
         throw error;
     }
 }
+
+// nomatch result is an empty array
+export async function postLyrics(bodyData) {
+    const options = {
+        mode: 'cors',
+        method: 'POST',
+        body: JSON.stringify(bodyData),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        }
+    };
+    try {
+        let result = await fetch('http://loppuprojekti-env.4wv6cxwtgr.eu-central-1.elasticbeanstalk.com/lyrics', options);
+        let jsonRes = await result.json();
+        if (jsonRes.success) {
+            return jsonRes.message;
+        } else {
+            throw new Error(jsonRes.message);
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}

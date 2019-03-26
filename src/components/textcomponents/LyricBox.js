0e634d234c79d795ec749d330a1c737debcf6fe9
jiiaa <jukkaa@live.com>
2019-03-26 16:47:12 +0200

    Find and bold the search string in lyrics /Jukka

--- src/components/textcomponents/LyricBox.js
+++ src/components/textcomponents/LyricBox.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import { Button, Collapse } from 'react-bootstrap';
+import Parser from 'html-react-parser';
+import './styles/LyricBox.css';
 
 class LyricBox extends Component {
     constructor(props, context) {
         super(props, context);
 
         this.state = {
-            open: false
+            open: false,
+            matched: 'mountain high'
         };
     }
 
     render() {
         const { open } = this.state;
+
+        let matchString = new RegExp(this.props.toMatch, "gi")
+        let matched = this.props.text.replace(matchString, "<span>" + this.props.toMatch + "</span>");
+
         return (
             <div>
                 <Button onClick={() => this.setState({ open: !open })} aria-controls="example-collapse-text" aria-expanded={open}>
                     Get lyrics
                 </Button>
                 <Collapse in={this.state.open}>
-                    <div id="example-collapse-text">{this.props.text}</div>
+                    <div id="example-collapse-text">{Parser(matched)}</div>
                 </Collapse>
             </div>
         );

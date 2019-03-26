aeaef5dd9a6bd3a60b5e17033642c926a903aaad
Juhanir <juhani.riisio@gmail.com>
2019-03-26 13:42:42 +0200

    navbar formatting

--- src/components/navigation/AppNavigation.js
+++ src/components/navigation/AppNavigation.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
-import { Navbar, NavDropdown, Nav, Dropdown, Col, Row, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './styles/AppNavigation.css';
 
 class AppNavigation extends Component {
-  render() {
-    return (
-      <Container>
-      <Navbar expand="lg">
-        <Navbar.Brand>
-              
-        <img
-        src="/tunesterr.png"
-        width="120"
-        height="50"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />  
-        </Navbar.Brand>   
-        <Dropdown alignRight>
-          <Dropdown.Toggle variant="light" id="dropdown-basic">
-            Navigation          
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown><Link to="/">Home</Link></Dropdown>
-            <Dropdown><Link to="/filetest">Test file</Link></Dropdown>
-            <Dropdown><Link to="/humrecord">Test hum</Link></Dropdown>
-            <Dropdown><Link to="/texttest">Test lyrics</Link></Dropdown>
-            <Dropdown><Link to="/about">About</Link></Dropdown>
-          </Dropdown.Menu>
-        </Dropdown>
-        
-      </Navbar>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Navbar.Brand>
+                        <Link to="/">
+                            <img src="/tunesterr.png" width="120" height="50" className="d-inline-block align-top" alt="React Bootstrap logo" />
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <Link className="custom-nav-link" to="/filetest">
+                                <Badge variant="secondary">Test File</Badge>
+                            </Link>
+                            <Link className="custom-nav-link" to="/humrecord">
+                                <Badge variant="secondary">Test Hum</Badge>
+                            </Link>
+                            <Link className="custom-nav-link" to="/texttest">
+                                <Badge variant="secondary">Test Lyrics</Badge>
+                            </Link>
+                            <Link className="custom-nav-link" to="/about">
+                                <Badge variant="secondary">About</Badge>
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
+                {/* <Navbar expand="lg" style={{ backgroundColor: 'white' }}>
+                    
+                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Navigation
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown>
+                                    <Link to="/">Home</Link>
+                                </Dropdown>
+                                <Dropdown>
+                                    <Link to="/filetest">Test file</Link>
+                                </Dropdown>
+                                <Dropdown>
+                                    <Link to="/humrecord">Test hum</Link>
+                                </Dropdown>
+                                <Dropdown>
+                                    <Link to="/texttest">Test lyrics</Link>
+                                </Dropdown>
+                                <Dropdown>
+                                    <Link to="/about">About</Link>
+                                </Dropdown>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Navbar.Collapse>
+                </Navbar> */}
+            </Container>
+        );
+    }
 }
 
 export default AppNavigation;
